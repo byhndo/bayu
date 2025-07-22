e4925b20fbc7a1f9a722cb232c6a16042471b2f2
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-22 10:27:33 +0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -8,3 +8,12 @@ const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes
 });
+
+scrollBehavior(to, from, savedPosition) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ top: 0, behavior: 'smooth' });
+      }, 100);
+    });
+  },
+});
