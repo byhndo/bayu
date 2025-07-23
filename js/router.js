2372d8af7e14a23e0d2cbaf6cd07e52430c9847d
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-23 10:49:33 +0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,7 +1,8 @@
 const routes = [
   { path: '/', redirect: '/bio' },
   { name: "Bio", path: "/bio", component: Bio },
-  { name: "Photos", path: "/photos", component: Photos }
+  { name: "Photos", path: "/photos", component: Photos },
+  { path: '/:pathMatch(.*)*', component: NotFound }
 ];
 
 const base = location.pathname.split('/')[1]
