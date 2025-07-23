2436f781a6c97f5a0772f3bbc96714161abf3c64
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-23 16:30:55 +0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,10 +1,11 @@
 const routes = [
   { path: '/', redirect: '/bio' },
   { name: "Bio", path: "/bio", component: Bio },
-  { name: "Photos", path: "/photos", component: Photos }
+  { name: "Photos", path: "/photos", component: Photos },
+  { path: '/:pathMatch(.*)*', redirect: '/' } 
 ];
 
 const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
+  history: VueRouter.createWebHistory(),
   routes
 });
