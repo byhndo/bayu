1d20a443e1b9d49fba39ef5b2d1e3b6fa2888471
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-27 21:21:19 +0700

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -101,7 +101,7 @@ Koleksi beberapa foto momen berharga, setidaknya untuk saya, yang berhasil saya 
 	
 <!-- footer -->
 <div class="box4">                          
-<div class="footer"><span>&copy;<a href="/"> Bayu Handono</a> All right reserved.</span>
+<div class="footer"><span>&copy;<a href="/"> Bayu Handono.</a> All right reserved.</span>
 </div>
 </div>
 <!-- footer -->
