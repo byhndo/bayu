8c153e106ed1d4ee8d142f5a37e1b73a7e182c08
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-05 00:09:17 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ const gulp = require('gulp');
 const obfuscator = require('gulp-javascript-obfuscator');
 const htmlmin = require('gulp-htmlmin');
 const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('js', () => {
-  return gulp.src('js/*.js')
-    .pipe(obfuscator({ compact: true }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/js'));
+  return gulp.src([
+    'js/bio.js',
+    'js/photos.js',
+    'js/router.js',
+    'js/app.js'
+  ])
+  .pipe(obfuscator({ compact: true }))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('html', () => {
@@ -16,4 +22,11 @@ gulp.task('html', () => {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.parallel('js', 'html'));
+gulp.task('css', () => {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', gulp.parallel('js', 'html', 'css'));
